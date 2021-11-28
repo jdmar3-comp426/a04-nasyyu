c60b890271ebcb8668cc408fc727f64e2ec93097
Natalie Yuziuk <59803404+nasyyu@users.noreply.github.com>
2021-11-27 23:50:39 -0500

    show up in actions pls

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT));
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
@@ -55,11 +55,11 @@ app.patch("/app/update/user/:id", (req, res) => {
 app.delete("/app/delete/user/:id", (req, res) => {	
 	var id = req.params.id;
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(id);
-	res.json({"message":`1 record deleted: ID ${id} (200)`})
+	res.json({"message":`1 record deleted: ID ${id} (200)`});
 	res.status(200);
 });
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Your API is working!"});
-    res.status(404);
+    res.status(404); 
 });
